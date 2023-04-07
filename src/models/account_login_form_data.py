49823dc27d3c69fd523f3bb2d94ac3bf272ca4f7
Eldos <eldos.baktybekov@gmail.com>
2023-04-07 11:18:16 +0600

    :construction: Split `AccountLoginFormData` model (#GI-28)
    * `EmptyAccountLoginFormData` contains only data from html page
     (`return_url`, `request_verification_token`).
     * `FilledAccountLoginFormData` has all field of `AccountLoginFormData`.

--- src/models/account_login_form_data.py
+++ src/models/account_login_form_data.py
@@ -1,15 +1,19 @@
 from dataclasses import dataclass
 
-__all__ = ('AccountLoginFormData',)
+__all__ = ('EmptyAccountLoginFormData', 'FilledAccountLoginFormData')
 
 
 @dataclass(frozen=True, slots=True)
-class AccountLoginFormData:
+class EmptyAccountLoginFormData:
     return_url: str
+    request_verification_token: str
+
+
+@dataclass(frozen=True, slots=True)
+class FilledAccountLoginFormData(EmptyAccountLoginFormData):
     username: str
     password: str
     tenant_name: str
     country_code: str
     auth_method: str
     remember_login: bool
-    request_verification_token: str
