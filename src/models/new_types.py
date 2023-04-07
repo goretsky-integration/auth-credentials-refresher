974752c907480c2344766a1a65de2f5074e25501
Eldos <eldos.baktybekov@gmail.com>
2023-04-07 18:30:20 +0600

    :sparkles: Add `HTML` type (#GI-28)

--- src/models/new_types.py
+++ src/models/new_types.py
@@ -2,6 +2,7 @@ from typing import NewType
 
 import httpx
 
-__all__ = ('HTTPClient',)
+__all__ = ('HTTPClient', 'HTML')
 
 HTTPClient = NewType('HTTPClient', httpx.Client)
+HTML = NewType('HTML', str)
