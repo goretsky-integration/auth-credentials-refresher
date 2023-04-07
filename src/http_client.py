a7f901d77ca8c12bcc9f293ee0f0ef117db9ca33
Eldos <eldos.baktybekov@gmail.com>
2023-04-07 22:14:02 +0600

    :sparkles: Add `timeout` parameter (#GI-28)

--- src/http_client.py
+++ src/http_client.py
@@ -8,10 +8,19 @@ __all__ = ('closing_http_client',)
 
 
 @contextlib.contextmanager
-def closing_http_client(*, base_url: str) -> models.HTTPClient:
+def closing_http_client(
+        *,
+        base_url: str,
+        timeout: int | float = 15,
+) -> models.HTTPClient:
     if not base_url:
         raise ValueError('Base url is not provided')
 
     headers = {'User-Agent': 'dodoextbot'}
-    with httpx.Client(base_url=base_url, headers=headers) as client:
+    with httpx.Client(
+            base_url=base_url,
+            headers=headers,
+            follow_redirects=True,
+            timeout=timeout,
+    ) as client:
         yield models.HTTPClient(client)
