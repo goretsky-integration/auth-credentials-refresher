ec59db3d272a66e99fb61755c0aa64df9379b760
Eldos <eldos.baktybekov@gmail.com>
2023-04-08 11:20:22 +0600

    :sparkles: Add `RoleId` enum (#GI-28)

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,6 +3,7 @@ from .account_login_form_data import *
 from .connect_authorize_form_data import *
 from .new_types import *
 from .office_manager_account import *
+from .roles import *
 from .select_department_form_data import *
 from .select_role_form_data import *
 from .sign_in_oidc_form_data import *
---
+++ src/models/roles.py
@@ -0,0 +1,8 @@
+from enum import IntEnum
+
+__all__ = ('RoleId',)
+
+class RoleId(IntEnum):
+    SHIFT_MANAGER = 3
+    OFFICE_MANAGER = 7
+    ACCOUNTANT = 16
