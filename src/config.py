0632be72ef969db496872257b930d66b693226f8
Eldos <eldos.baktybekov@gmail.com>
2023-04-09 20:14:54 +0600

    :wrench: Change config parser (#GI-28)

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 import pathlib
 import tomllib
 from dataclasses import dataclass
-from uuid import UUID
 
 __all__ = (
     'Config',
-    'load_config',
+    'read_toml_config_string',
+    'read_toml_config_file',
 )
 
 
@@ -13,21 +13,20 @@ __all__ = (
 class Config:
     auth_base_url: str
     office_manager_base_url: str
-    username: str
-    password: str
-    role_id: int
-    department_uuid: UUID
+    country_code: str
+    remember_login: bool
 
 
-def load_config(file_path: str | pathlib.Path) -> Config:
-    with open(file_path, 'rb') as file:
-        raw_config = tomllib.load(file)
-
+def read_toml_config_string(config_string: str) -> Config:
+    raw_config = tomllib.loads(config_string)
     return Config(
-        auth_base_url=raw_config['base_url']['auth'],
-        office_manager_base_url=raw_config['base_url']['office_manager'],
-        username=raw_config['auth_credentials']['username'],
-        password=raw_config['auth_credentials']['password'],
-        role_id=raw_config['auth_credentials']['role_id'],
-        department_uuid=UUID(raw_config['auth_credentials']['department_uuid']),
+        auth_base_url=raw_config['auth_base_url'],
+        office_manager_base_url=raw_config['office_manager_base_url'],
+        country_code=raw_config['country_code'],
+        remember_login=raw_config['remember_login'],
     )
+
+
+def read_toml_config_file(file_path: pathlib.Path) -> Config:
+    config_string = file_path.read_text(encoding='utf-8')
+    return read_toml_config_string(config_string)
